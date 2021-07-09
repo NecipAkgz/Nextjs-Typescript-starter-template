10ffa05a4f4d48d2b962bb16f35f9b8d6a27ebe3
Necip <nexco@hotmail.com>
2021-07-09 03:41:27 +0300

    eslint error, warm ile degistirildi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
+  <>
+    <DefaultSeo
+      openGraph={{
+        type: "Yblşalsk",
+        locale: 'tr-TR',
+        url: 'www.google.com',
+        site_name: 'Çiftlik_Gang',
+      }}
+    />
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
